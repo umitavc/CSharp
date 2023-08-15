22e880b08a3ac51ac52bf17b070c147d7f45051d
umitavc <umitavc62@gmail.com>
2023-08-15 17:25:58 +0300

    zor seviye projeler eklendi

---
+++ atm-uygulamasi/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace ATMUygulamasi
+{
+    class Program
+    {
+        static string kullaniciAdi = "kullanici";
+        static string parola = "sifre";
+        static decimal bakiye = 1000;
+
+        static List<string> islemListesi = new List<string>
+        {
+            "1. Para Çekme",
+            "2. Para Yatırma",
+            "3. Ödeme Yapma",
+            "4. Gün Sonu İşlemi",
+            "5. Çıkış"
+        };
+
+        static void Main(string[] args)
+        {
+            GirisYap();
+
+            bool devam = true;
+            while (devam)
+            {
+                MenuGoster();
+
+                int secim = Convert.ToInt32(Console.ReadLine());
+
+                switch (secim)
+                {
+                    case 1:
+                        ParaCek();
+                        break;
+                    case 2:
+                        ParaYatir();
+                        break;
+                    case 3:
+                        OdemeYap();
+                        break;
+                    case 4:
+                        GunSonuIslemi();
+                        break;
+                    case 5:
+                        devam = false;
+                        break;
+                    default:
+                        Console.WriteLine("Geçersiz seçim.");
+                        break;
+                }
+            }
+        }
+
+        static void GirisYap()
+        {
+            Console.Write("Kullanıcı Adı: ");
+            string girilenKullaniciAdi = Console.ReadLine();
+
+            Console.Write("Parola: ");
+            string girilenParola = Console.ReadLine();
+
+            if (girilenKullaniciAdi == kullaniciAdi && girilenParola == parola)
+            {
+                Console.WriteLine("Giriş başarılı. Hoş geldiniz!");
+            }
+            else
+            {
+                Console.WriteLine("Hatalı giriş. Program sonlandırılıyor.");
+                Environment.Exit(0);
+            }
+        }
+
+        static void MenuGoster()
+        {
+            Console.WriteLine("İşlem Seçenekleri:");
+            foreach (string islem in islemListesi)
+            {
+                Console.WriteLine(islem);
+            }
+            Console.Write("Lütfen bir işlem seçin: ");
+        }
+
+        static void ParaCek()
+        {
+            Console.Write("Çekmek istediğiniz miktarı girin: ");
+            decimal cekilenMiktar = Convert.ToDecimal(Console.ReadLine());
+
+            if (cekilenMiktar <= bakiye)
+            {
+                bakiye -= cekilenMiktar;
+                Console.WriteLine("Para çekme işlemi başarılı. Yeni bakiye: " + bakiye);
+            }
+            else
+            {
+                Console.WriteLine("Yetersiz bakiye.");
+            }
+        }
+
+        static void ParaYatir()
+        {
+            Console.Write("Yatırmak istediğiniz miktarı girin: ");
+            decimal yatirilanMiktar = Convert.ToDecimal(Console.ReadLine());
+
+            bakiye += yatirilanMiktar;
+            Console.WriteLine("Para yatırma işlemi başarılı. Yeni bakiye: " + bakiye);
+        }
+
+        static void OdemeYap()
+        {
+            Console.WriteLine("Ödeme yapma işlemi seçildi.");
+            
+        }
+
+        static void GunSonuIslemi()
+        {
+            Console.WriteLine("Gün sonu işlemi seçildi.");
+
+            
+            string loglar = "Transaction Logları:\n..."; 
+            loglar += "\n\nHatalı Giriş Denemeleri:\n..."; 
+
+          
+            string dosyaAdi = DateTime.Now.ToString("ddMMyyyy") + ".txt";
+            string dosyaYolu = Path.Combine(Directory.GetCurrentDirectory(), dosyaAdi);
+            File.WriteAllText(dosyaYolu, loglar);
+
+            Console.WriteLine("Gün sonu işlemi tamamlandı. Loglar dosyaya kaydedildi: " + dosyaYolu);
+        }
+    }
+}
---
+++ voting-uygulamasi/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+Dictionary<string, int> categoryVotes = new Dictionary<string, int>();
+Dictionary<string, string> users = new Dictionary<string, string>();
+
+while (true)
+{
+    Console.WriteLine("Hoşgeldiniz!");
+    Console.WriteLine("1. Oy Ver");
+    Console.WriteLine("2. Sonuçları Görüntüle");
+    Console.WriteLine("3. Çıkış");
+    Console.Write("Seçiminizi yapın: ");
+    string choice = Console.ReadLine();
+
+    switch (choice)
+    {
+        case "1":
+            Vote();
+            break;
+        case "2":
+            ShowResults();
+            break;
+        case "3":
+            Console.WriteLine("Uygulama kapatılıyor...");
+            return;
+        default:
+            Console.WriteLine("Geçersiz seçenek!");
+            break;
+    }
+}
+
+void Vote()
+{
+    Console.Write("Kullanıcı adınızı girin: ");
+    string username = Console.ReadLine();
+
+    if (!users.ContainsKey(username))
+    {
+        Console.WriteLine("Yeni kullanıcı kaydı oluşturuluyor...");
+        users[username] = username;
+    }
+
+    Console.WriteLine("Mevcut kategoriler: Film, Tech Stack, Spor");
+    Console.Write("Oy vermek istediğiniz kategori: ");
+    string category = Console.ReadLine();
+
+    if (!categoryVotes.ContainsKey(category))
+    {
+        categoryVotes[category] = 0;
+    }
+
+    categoryVotes[category]++;
+
+    Console.WriteLine("Oyunuz başarıyla kaydedildi!");
+}
+
+void ShowResults()
+{
+    Console.WriteLine("Voting Sonuçları:");
+    foreach (var category in categoryVotes)
+    {
+        Console.WriteLine($"{category.Key}: {category.Value} oy");
+    }
+
+    int totalVotes = 0;
+    foreach (var votes in categoryVotes.Values)
+    {
+        totalVotes += votes;
+    }
+
+    Console.WriteLine($"Toplam oy sayısı: {totalVotes}");
+
+    foreach (var category in categoryVotes)
+    {
+        double percentage = (double)category.Value / totalVotes * 100;
+        Console.WriteLine($"{category.Key} oy yüzdesi: {percentage:F2}%");
+    }
+}
